bae9e482202c5bf13f37ee5210e9e500b61b4d37
teja-akella <teja@101gen.ai>
2025-07-07 10:10:59 +0000

    Initial commit

---
+++ tests/utils.py
@@ -0,0 +1,45 @@
+import os
+import yaml
+import pytest
+
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as chromeOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+@pytest.fixture(scope="session")
+def setup():
+    chrome_options = chromeOptions()
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.maximize_window()
+    driver.quit()
+
+def login(setup):
+    driver = setup
+    with open('..\tests\credentials.yaml', 'r') as file:
+        credentials = yaml.safe_load(file)
+
+        #step 1: Open the browser and navigate to the login page
+        driver.get("http://localhost:5173/login")
+        wait = WebDriverWait(driver, 30)
+        
+        #step 2: Enter the username and password
+        email = driver.find_element(By.xpath, "//input[@name='username']")
+        password = driver.find_element(By.xpath, "//input[@name='password']")
+
+        email.send_keys(credentials['qa_username'])
+        password.send_keys(credentials['qa_password'])
+
+        #step 3: Click the login button
+        login_button = wait.until(EC.element_to_be_clickable((By.xpath, "//span[text()='Login']/..")))
+
+        login_button.click()
+
+        #step 4: Wait for the page to load and verify the login was successful
+        wait.until(EC.element_to_be_located((By.xpath, "//div[text()='Welcome to 101GenAI!]")))
+        assert "Welcome to 101GenAI!" in driver.page_source, "Login failed or page did not load correctly."
+        print("Login successful!")
+        time.sleep(2)
+
+
